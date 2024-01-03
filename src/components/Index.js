ff9f3995df61f8e0b0def02c105ae539028a9b26
Aman <ak193566@gmail.com>
2024-01-03 18:45:32 +0530

    Update Index.js

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,7 +1,7 @@
 import Header from "./Header/Header";
 import Home from "./Home/Home";
 import Footer from "./Footer/Footer";
-import LogIn from "./LogIn/Login";
+import LogIn from "./LogIn/LogIn";
 import SignUp from "./SignIn/SignIn";
 
 export {Header, Home, Footer, LogIn, SignUp};
