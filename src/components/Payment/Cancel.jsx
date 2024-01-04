c39cb7e622c658c5cab8e15b12f73276e1c30f63
Aman <ak193566@gmail.com>
2024-01-04 13:24:51 +0530

    add styling to success and cancel components

--- src/components/Payment/Cancel.jsx
+++ src/components/Payment/Cancel.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 function Cancel() {
   return (
-    <div>Cancel</div>
+    <>
+      <div className='flex flex-col justify-center items-center h-[100vh] bg-gradient-to-b from-gray-900 to-red-500 px-4'>
+      <h1 className='text-3xl pb-6 italic text-white text-center'>Your Payment is Cancelled</h1>
+      <h1 className='text-3xl pb-6 italic text-white text-center'>Return to Home</h1>
+      <NavLink to="/">
+        <button className='text-xl bg-black px-4 py-2 rounded-xl hover:bg-transparent text-red-500 hover:border-4 hover:border-white hover:text-white hover:font-semibold'>Home</button>
+      </NavLink>
+    </div>
+    </>
   )
 }
 
--- src/components/Payment/Success.jsx
+++ src/components/Payment/Success.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 function Success() {
   return (
-    <div>Success</div>
+    <>
+    <div className='flex flex-col justify-center items-center h-[100vh] bg-gradient-to-b from-gray-900 to-red-500 px-4'>
+      <h1 className='text-3xl pb-6 italic text-white text-center'>Your Payment is Successfully Done</h1>
+      <h1 className='text-3xl pb-6 italic text-white text-center'>Your order has been placed.</h1>
+      <h1 className='text-3xl pb-6 italic text-white text-center'>Return to Home</h1>
+      <NavLink to="/">
+        <button className='text-xl bg-black px-4 py-2 rounded-xl hover:bg-transparent text-red-500 hover:border-4 hover:border-white hover:text-white hover:font-semibold'>Home</button>
+      </NavLink>
+    </div>
+    </>
   )
 }
 
