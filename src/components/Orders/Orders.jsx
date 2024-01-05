9e00f5644f90798f2e54a126fbf6c44f66b53282
Aman <ak193566@gmail.com>
2024-01-05 20:42:32 +0530

    update

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import appwriteService from '../../appwrite/database';
 import toast from "react-hot-toast";
 import authService from '../../appwrite/auth';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, NavLink } from 'react-router-dom';
 
 function Orders() {
 const navigate = useNavigate();
@@ -68,119 +68,177 @@ const navigate = useNavigate();
     }
   return (
     <>
-      <div className="flex flex-col ">
-        <h1
-          onClick={() => {
-            navigate("/");
-            window.location.reload();
-          }}
-        >
-          SneakWear
-        </h1>
-        <h2>Your Orders</h2>
-      </div>
-      {/* Fav */}
-      <div className="flex flex-col items-center justify-center mt-2 sm:mt-[2rem] mx-[2rem]">
-        <h1 className="text-3xl font-semibold">Favourites</h1>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 2xl:grid-cols-6">
-          {favList.map((data, index) => {
-            if (userDetails?.$id == data.$permissions[0].slice(11, -2)) {
-              return (
-                <section
-                  key={index}
-                  className="card bg-white rounded-lg my-[5px] mx-[2px] sm:my-[10px] sm:mx-[10px] border-2 border-[#ededed] p-[8px] sm:p-[20px] flex flex-col justify-end items-center"
-                >
-                  <img
-                    src={data.img}
-                    alt={data.title}
-                    className="card-img w-[80%] sm:w-[13rem] mb-2 sm:mb-[1rem]"
-                  />
-                  <div className="card-details my-2 w-[90%]">
-                    <h3 className="card-title sm:mb-[1rem] text-lg sm:text-[1.17em] font-bold w-[100%] sm:w-auto ">
-                      {data.title}
-                    </h3>
-                    <section className="card-price flex justify-between items-center">
-                      <div className="price text-[15px] sm:text-[16px]">
-                        <p className="inline-block ms-2"> ${data.newPrice}</p>
-                      </div>
-                      <div className="bag">
-                        <i className="ri-shopping-bag-fill text-[#535353]"></i>
-                      </div>
-                    </section>
-                    <div className="flex justify-between mt-2">
-                      <div className="flex items-center">
-                        <p>Qty.</p>
-                        <p className="mx-4">{data.noItems}</p>
-                      </div>
-                      <div>
-                        <button
-                          className="bg-red-500 py-1 px-2 text-xs text-white rounded-lg hover:bg-red-600"
-                          onClick={() => {
-                            removeFav(data.$id);
-                            toast.success("Fav. Item Removed");
-                          }}
-                        >
-                          Remove
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </section>
-              );
-            }
-          })}
+      <div className="flex flex-col bg-gradient-to-b from-gray-900 to-red-500 items-center">
+        <div className="relative w-full flex justify-center items-center mt-6">
+          <button
+            className="absolute left-5 md:left-10 text-white"
+            onClick={() => {
+              navigate("/");
+              window.location.reload();
+            }}
+          >
+            <i class="ri-arrow-left-line"></i>Back
+          </button>
+          <NavLink
+            // to="/"
+            onClick={() => {
+              navigate("/");
+              window.location.reload();
+            }}
+            className=""
+          >
+            <h1 className="text-3xl text-gray-300 font-semibold">Sneakwear</h1>
+          </NavLink>
         </div>
-      </div>
-      {/* Orders */}
-      <div className="flex flex-col items-center justify-center mt-2 sm:mt-[2rem] mx-[2rem]">
-        <h1 className="text-3xl font-semibold">Previous Orders</h1>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 2xl:grid-cols-6">
-          {ordersList.map((data, index) => {
-            if (userDetails?.$id == data.$permissions[0].slice(11, -2)) {
-              return (
-                <section
-                  key={index}
-                  className="card bg-white rounded-lg my-[5px] mx-[2px] sm:my-[10px] sm:mx-[10px] border-2 border-[#ededed] p-[8px] sm:p-[20px] flex flex-col justify-end items-center"
+        {/* Fav */}
+        <div className="flex flex-col items-center justify-center mt-2 sm:mt-[2rem] mx-[2rem]">
+          <h1 className="text-2xl font-semibold text-gray-300">
+            Favourites List
+          </h1>
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 2xl:grid-cols-6">
+            {userDetails ? (
+              <>
+                {favList.map((data, index) => {
+                  if (userDetails?.$id == data.$permissions[0].slice(11, -2)) {
+                    return (
+                      <section
+                        key={index}
+                        className="card bg-white rounded-lg my-[5px] mx-[2px] sm:my-[10px] sm:mx-[10px] border-2 border-[#ededed] p-[8px] sm:p-[20px] flex flex-col justify-end items-center"
+                      >
+                        <img
+                          src={data.img}
+                          alt={data.title}
+                          className="card-img w-[80%] sm:w-[13rem] mb-2 sm:mb-[1rem]"
+                        />
+                        <div className="card-details my-2 w-[90%]">
+                          <h3 className="card-title sm:mb-[1rem] text-lg sm:text-[1.17em] font-bold w-[100%] sm:w-auto ">
+                            {data.title}
+                          </h3>
+                          <section className="card-price flex justify-between items-center">
+                            <div className="price text-[15px] sm:text-[16px]">
+                              <p className="inline-block ms-2">
+                                {" "}
+                                ${data.newPrice}
+                              </p>
+                            </div>
+                            <div className="bag">
+                              <i className="ri-shopping-bag-fill text-[#535353]"></i>
+                            </div>
+                          </section>
+                          <div className="flex justify-between mt-2">
+                            <div className="flex items-center">
+                              <p>Qty.</p>
+                              <p className="mx-4">{data.noItems}</p>
+                            </div>
+                            <div>
+                              <button
+                                className="bg-red-500 py-1 px-2 text-xs text-white rounded-lg hover:bg-red-600"
+                                onClick={() => {
+                                  removeFav(data.$id);
+                                  toast.success("Fav. Item Removed");
+                                }}
+                              >
+                                Remove
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </section>
+                    );
+                  }
+                })}
+              </>
+            ) : (
+              <>
+                <p className="text-xl italic text-white text-center">
+                  Please Login First
+                </p>
+                <button
+                  onClick={() => {
+                    navigate("/login");
+                  }}
+                  className="text-xl bg-black px-4 py-2 rounded-xl hover:bg-transparent text-red-500 hover:border-4 hover:border-white hover:text-white hover:font-semibold"
                 >
-                  <img
-                    src={data.img}
-                    alt={data.title}
-                    className="card-img w-[80%] sm:w-[13rem] mb-2 sm:mb-[1rem]"
-                  />
-                  <div className="card-details my-2 w-[90%]">
-                    <h3 className="card-title sm:mb-[1rem] text-lg sm:text-[1.17em] font-bold w-[100%] sm:w-auto ">
-                      {data.title}
-                    </h3>
-                    <section className="card-price flex justify-between items-center">
-                      <div className="price text-[15px] sm:text-[16px]">
-                        <p className="inline-block ms-2"> ${data.newPrice}</p>
-                      </div>
-                      <div className="bag">
-                        <i className="ri-shopping-bag-fill text-[#535353]"></i>
-                      </div>
-                    </section>
-                    <div className="flex justify-between mt-2">
-                      <div className="flex items-center">
-                        <p>Qty.</p>
-                        <p className="mx-4">{data.noItems}</p>
-                      </div>
-                      <div>
-                        <button
-                          className="bg-red-500 py-1 px-2 text-xs text-white rounded-lg hover:bg-red-600"
-                          onClick={() => {
-                            removeOrders(data.$id);
-                            toast.success("Order Item Removed");
-                          }}
-                        >
-                          Remove
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </section>
-              );
-            }
-          })}
+                  Login
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+        {/* Orders */}
+        <div className="flex flex-col items-center justify-center mt-2 sm:mt-[2rem] mx-[2rem]">
+          <h1 className="text-3xl font-semibold text-gray-300">
+            Placed / Cancelled Orders
+          </h1>
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 2xl:grid-cols-6">
+            {userDetails ? (
+              <>
+                {ordersList.map((data, index) => {
+                  if (userDetails?.$id == data.$permissions[0].slice(11, -2)) {
+                    return (
+                      <section
+                        key={index}
+                        className="card bg-white rounded-lg my-[5px] mx-[2px] sm:my-[10px] sm:mx-[10px] border-2 border-[#ededed] p-[8px] sm:p-[20px] flex flex-col justify-end items-center"
+                      >
+                        <img
+                          src={data.img}
+                          alt={data.title}
+                          className="card-img w-[80%] sm:w-[13rem] mb-2 sm:mb-[1rem]"
+                        />
+                        <div className="card-details my-2 w-[90%]">
+                          <h3 className="card-title sm:mb-[1rem] text-lg sm:text-[1.17em] font-bold w-[100%] sm:w-auto ">
+                            {data.title}
+                          </h3>
+                          <section className="card-price flex justify-between items-center">
+                            <div className="price text-[15px] sm:text-[16px]">
+                              <p className="inline-block ms-2">
+                                {" "}
+                                ${data.newPrice}
+                              </p>
+                            </div>
+                            <div className="bag">
+                              <i className="ri-shopping-bag-fill text-[#535353]"></i>
+                            </div>
+                          </section>
+                          <div className="flex justify-between mt-2">
+                            <div className="flex items-center">
+                              <p>Qty.</p>
+                              <p className="mx-4">{data.noItems}</p>
+                            </div>
+                            <div>
+                              <button
+                                className="bg-red-500 py-1 px-2 text-xs text-white rounded-lg hover:bg-red-600"
+                                onClick={() => {
+                                  removeOrders(data.$id);
+                                  toast.success("Order Item Removed");
+                                }}
+                              >
+                                Remove
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </section>
+                    );
+                  }
+                })}
+              </>
+            ) : (
+              <>
+                <p className="text-xl italic text-white text-center">
+                  Please Login First
+                </p>
+                <button
+                  onClick={() => {
+                    navigate("/login");
+                  }}
+                  className="text-xl bg-black px-4 py-2 rounded-xl hover:bg-transparent text-red-500 hover:border-4 hover:border-white hover:text-white hover:font-semibold"
+                >
+                  Login
+                </button>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </>
